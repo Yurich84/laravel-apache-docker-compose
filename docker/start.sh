18bd1278cc673bc651b63ce3d231bdb90ab478e5
Yurii Tymchuk <yurii.tymchuk@skyup.studio>
2019-12-24 13:46:33 +0200

    add schedule and queue

--- docker/start.sh
+++ docker/start.sh
@@ -4,22 +4,27 @@ set -e
 
 role=${CONTAINER_ROLE:-app}
 env=${APP_ENV:-production}
+path=${APP_PATH_CONTAINER:-/var/www/html}
 
 if [ "$env" != "local" ]; then
     echo "Caching configuration..."
-    (cd /application && php artisan config:cache && php artisan route:cache && php artisan view:cache)
+    (cd "$path" && php artisan config:cache && php artisan route:cache && php artisan view:cache)
 fi
 
-if [ "$role" = "queue" ]; then
+if [ "$role" = "app" ]; then
+
+    exec apache2-foreground
+
+elif [ "$role" = "queue" ]; then
 
     echo "Running the queue..."
-    php /application/artisan queue:work --verbose --tries=3 --timeout=90
+    php "$path"/artisan queue:work --verbose --tries=3 --timeout=90
 
 elif [ "$role" = "scheduler" ]; then
 
     while [ true ]
     do
-      php /application/artisan schedule:run --verbose --no-interaction &
+      php "$path"/artisan schedule:run --verbose --no-interaction &
       sleep 60
     done
 
